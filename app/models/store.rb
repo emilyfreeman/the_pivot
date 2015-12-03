3c06dd8fa9bba27cd2e3b9aaa921e5ceba4a7054
Emily Dowdle <emily@editingemily.com>
2015-12-03 15:39:52 -0700

    Update user store relationship

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
-  belongs_to :user
-  has_many :user_stores
-  has_many :users, through: :user_stores
+  has_many :users
+  # has_many :user_stores
+  # has_many :users, through: :user_stores
   has_many :items
 
   def categories(store)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
-
-  has_many :user_stores
-  has_many :stores, through: :user_stores
+  belongs_to :store
+  # has_many :user_stores
+  # has_many :stores, through: :user_stores
 
   has_many :user_roles
   has_many :roles, through: :user_roles
