06b2b66eaadeabf66e5ba409bb310154e397dcde
Emily Dowdle <emily@editingemily.com>
2015-12-03 10:37:48 -0700

    Change title of original integration tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151202211307) do
+ActiveRecord::Schema.define(version: 20151203021536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
