10dd1ebf1a34258c497dd9ebe0ca646900ba86af
Justin Pease <justinpease2@gmail.com>
2015-12-03 16:52:10 -0700

    Add User Profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203021536) do
+ActiveRecord::Schema.define(version: 20151203234920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20151203021536) do
     t.datetime "updated_at",      null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "bio"
   end
 
   add_foreign_key "item_orders", "items"
