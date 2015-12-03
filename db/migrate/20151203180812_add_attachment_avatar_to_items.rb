c586f26865d85f6d47f42f025e75fc87d99931e2
Emily Dowdle <emily@editingemily.com>
2015-12-03 16:07:15 -0700

    Remove avatar columns from user and item tables

--- db/migrate/20151203180812_add_attachment_avatar_to_items.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentAvatarToItems < ActiveRecord::Migration
-  def self.up
-    add_attachment :items, :avatar
-  end
-
-  def self.down
-    remove_attachment :items, :avatar
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203180812) do
+ActiveRecord::Schema.define(version: 20151203220649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +110,10 @@ ActiveRecord::Schema.define(version: 20151203180812) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "store_id"
   end
+
+  add_index "users", ["store_id"], name: "index_users_on_store_id", using: :btree
 
   add_foreign_key "item_orders", "items"
   add_foreign_key "item_orders", "orders"
