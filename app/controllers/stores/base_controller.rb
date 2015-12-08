6f491a07a24b1b6fde09f4355f000e9f42e85c1f
Justin Pease <justinpease2@gmail.com>
2015-12-07 22:53:16 -0700

    John fixed it
    user can view his store dashboard page and see the stores orders

--- app/controllers/stores/base_controller.rb
+++ app/controllers/stores/base_controller.rb
@@ -1,3 +1,4 @@
 class Stores::BaseController < ApplicationController
 
+
 end
--- app/controllers/stores/dashboard_controller.rb
+++ app/controllers/stores/dashboard_controller.rb
@@ -3,4 +3,9 @@ class Stores::DashboardController < Stores::BaseController
     @store = current_user.store
     @orders = Order.desc_by_date
   end
+
+  def show
+    @store = current_user.store
+    @orders = Order.desc_by_date
+  end
 end
