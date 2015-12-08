291c2bf2c001d58cd74b209b4a782765505eef90
Emily Dowdle <emily@editingemily.com>
2015-12-08 14:48:07 -0700

    Update admins controller

--- app/controllers/stores/admins_controller.rb
+++ app/controllers/stores/admins_controller.rb
@@ -1,8 +1,9 @@
 class Stores::AdminsController < Stores::BaseController
 
+  before_action :current_admin?
+
   def new
     @users = User.all
-    # respond_with @users
   end
 
   def create
@@ -16,9 +17,8 @@ class Stores::AdminsController < Stores::BaseController
   end
 
   def destroy
-    @admin_to_be_deleted = User.find(params[:id])
-    current_user.store.users.delete(@admin_to_be_deleted)
-    @admin_to_be_deleted.store = nil
+    current_user.store.users.find(params[:id]).update(store_id: nil)
     redirect_to store_dashboard_index_path
   end
+
 end
