fac3bfd674d1944a1a9f900bfded4d05f001b50e
Justin Pease <justinpease2@gmail.com>
2015-12-08 18:02:32 -0700

    happy path for user log in

--- test/integration/login_test_test.rb
+++ test/integration/login_test_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class VisitorRegisterAndCreateProfileTest < ActionDispatch::IntegrationTest
+class LoginTest < ActionDispatch::IntegrationTest
 
   test "a non registered user can not log in" do
     visit '/'
@@ -11,6 +11,12 @@ class VisitorRegisterAndCreateProfileTest < ActionDispatch::IntegrationTest
 
     assert_equal current_path, login_path
     assert page.has_content?("Invalid Login. Try Again.")
+  end
+
+  test "a registered user can log in" do
+    create_user
+    login_user
+    assert page.has_content?("Logged in as")
   end
 
 end
