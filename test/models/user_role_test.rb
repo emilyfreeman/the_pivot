c5e94441633fa340924f84ec8fbb747073cdf66b
John Slota <john.slota@gmail.com>
2015-12-08 20:54:32 -0700

    dump

--- test/models/user_role_test.rb
+++ test/models/user_role_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
 
 class UserRoleTest < ActiveSupport::TestCase
-  assert_equal
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,5 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  binding.pry
-  roles.exists?(name: "registered_user")
 
 end
