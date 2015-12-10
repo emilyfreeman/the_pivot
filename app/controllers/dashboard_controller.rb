d63bcb2eca13fcc2af6531982d4015a496ad5e7d
Justin Pease <justinpease2@gmail.com>
2015-12-09 23:32:44 -0700

    Closes #13-Platform-admin-can-take-a-business-offline

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,4 @@ class DashboardController < ApplicationController
     @store = current_user.store
     @orders = @store.orders
   end
-
-  # def show
-  #   @store = current_user.store
-  #   @orders = Order.desc_by_date
-  # end
 end
