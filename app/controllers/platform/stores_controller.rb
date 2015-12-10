446d04b1f74095b15d25d168eba56a0e7ba74a68
Justin Pease <justinpease2@gmail.com>
2015-12-10 11:20:48 -0700

    should be the last

--- app/controllers/platform/stores_controller.rb
+++
@@ -1,7 +0,0 @@
-class Platform::StoresController < ApplicationController
-  def update
-     @store = Store.find(params[:id])
-     @store.update_attributes(status: params[:edit_action])
-     redirect_to platform_dashboard_index_path
-  end
-end
